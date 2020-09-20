642b581f60a708ab5dc68998580dd48db2483383
Maxime Larrieu <maxime.larrieu@ynov.com>
2020-09-21 00:11:22 +0200

    :sparkles: add roles and profile to users

--- app/User.php
+++ app/User.php
@@ -5,10 +5,16 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRoles;
+
+    public function profile() {
+        return $this->hasOne('App\Profile');
+    }
 
     /**
      * The attributes that are mass assignable.
